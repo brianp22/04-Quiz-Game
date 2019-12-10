04e02fd867e08274c518be6fe4e4451fe6c8a616
Brian Prickril <bprickril22@gmail.com>
2019-12-10 18:55:42 -0500

    successfully queued the first question

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 var startButton = document.getElementById("start-btn");
 var questionContainer = document.getElementById("question-container")
+var questionElement = document.getElementById("question");
+var answerButtonsElement = document.getElementById("answer-buttons");
 
 startButton.addEventListener("click", startGame);
 
 function startGame() {
     console.log("started")
     startButton.classList.add("hide");
+    currentQuestionIndex = 0;
     questionContainer.classList.remove("hide")
     nextQuestion();
-    
-    
-
-
 }
 
 function nextQuestion () {
+    showQuestion(questions[currentQuestionIndex])
 
 }
+
+function showQuestion(questions) {
+    questionElement.innerHTML = questions.title;
+
+}
+
