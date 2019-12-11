f9fee2c48f4724b614b8bec22ecc05507073f7e9
Brian Prickril <bprickril22@gmail.com>
2019-12-10 19:32:40 -0500

    populating buttons with correct choices

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var startButton = document.getElementById("start-btn");
+var nextButton = document.getElementById("next-btn");
 var questionContainer = document.getElementById("question-container")
 var questionElement = document.getElementById("question");
 var answerButtonsElement = document.getElementById("answer-buttons");
+var choices = questions.choices
 
 startButton.addEventListener("click", startGame);
 
@@ -14,12 +16,31 @@ function startGame() {
 }
 
 function nextQuestion () {
-    showQuestion(questions[currentQuestionIndex])
+    resetPage()
+    showQuestion(questions[currentQuestionIndex]);
 
 }
 
 function showQuestion(questions) {
     questionElement.innerHTML = questions.title;
+    for (var i = 0; i < questions.choices.length; i++) {
+        var button = document.createElement("button");
+        button.innerHTML = questions.choices[i];
+        button.classList.add("btn")
+        button.addEventListener("click", selectAnswer);
+        answerButtonsElement.appendChild(button);
+    }
 
+}
+
+function selectAnswer(e) {
+
+}
+
+function resetPage() {
+    nextButton.classList.add("hide");
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
 }
 
